a88a04b2c6e8c582c8fca70d7d07d896d84c68d7
inferman <infernal.manowar@gmail.com>
2019-07-29 17:07:10 +0300

    create new instance method

--- app.js
+++ app.js
@@ -1,7 +1,49 @@
 const budgetController = (function() {
 
+  let Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  let Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  const data = {
+    allItems: {
+      inc: [],
+      exp: []
+    },
+    totals: {
+      get inc() {
+        return data.allItems.inc.length || 0
+      },
+      get exp() {
+        return data.allItems.exp.length || 0
+      }
+    }
+  }
+
   return {
-    
+    addItem: (type, des, val) => {
+      const ID = 
+        data.allItems[type].length
+        ? data.allItems[type][data.allItems[type].length - 1].id + 1
+        : 0;
+
+      const newItem =
+        type === 'exp' 
+        ? new Expense(ID, des, val)
+        : new Income(ID, des, val);
+
+      data.allItems[type].push(newItem);
+      
+      return newItem;
+    },
+    testing: _ => { console.log(data) }
   }
 })();
 
@@ -40,14 +82,19 @@ const controller = (function(budgetCtrl, UICtrl) {
   };
 
   const ctrlAddItem = function() { 
-    const input = UICtrl.getInputs();
-    console.log(input);    
+    let input, newItem;
+    input = UICtrl.getInputs();
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+    budgetCtrl.testing();
   }
   
   return {
     init: () => {
       setEventListeners();
-    }
+    },
+    addNewItem: () => {
+      
+    } 
   }
 })(budgetController, UIController);
 
